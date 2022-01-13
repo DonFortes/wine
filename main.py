8dfce5d0b07f63fe9d7a1db46a190df41123e147
Павел <paul-36@mail.ru>
2022-01-13 20:01:57 +0300

    Naming fixed

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import datetime
 from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from pprint import pprint
 
 import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 START_YEAR = 1920
-FILE_NAME = "wine3.xlsx"
+FILE_NAME = "excel_files/wine3.xlsx"
 SHEET_NAME = "Лист1"
 
 
@@ -18,38 +19,42 @@ class Server:
         self.template = self.env.get_template("template.html")
         self.start_year = START_YEAR
         self.year_now = datetime.datetime.now().year
-        self.delta = self.year_now - self.start_year
+        self.age = self.year_now - self.start_year
 
     def start_server_and_create_index(self, excel):
         rendered_page = self.template.render(
-            year=self.delta,
-            categories=excel.create_default_dict_to_render_page(),
+            winery_age=self.age,
+            grouped_wines=excel.get_grouped_wines(),
         )
         with open("index.html", "w", encoding="utf8") as file:
             file.write(rendered_page)
-        server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-        return server.serve_forever()
+
+    def main(self):
+        _server = HTTPServer(("0.0.0.0", 8001), SimpleHTTPRequestHandler)
+        return _server.serve_forever()
 
 
 class Excel:
     def __init__(self):
         self.excel_name = FILE_NAME
         self.sheet_name = SHEET_NAME
-        self.data_frame_object = pandas.read_excel(
+        self.wines = pandas.read_excel(
             self.excel_name,
             self.sheet_name,
             na_filter=False,
-        )
-        self.excel_data_dict = self.data_frame_object.to_dict(orient="records")
-
-    def create_default_dict_to_render_page(self):
-        dict_of_lists = defaultdict(list)
-        for wine in self.excel_data_dict:
-            dict_of_lists[wine["Категория"]].append(wine)
-        return dict_of_lists
+        ).to_dict(orient="records")
+        # Меня просто учили, что дробить на части - это лучше, чем
+        # делать многосоставные "предложения". Проще дебажить в случае чего
+    def get_grouped_wines(self):
+        wines_sorted_by_group = defaultdict(list)
+        for wine in self.wines:
+            wines_sorted_by_group[wine["Категория"]].append(wine)
+        return wines_sorted_by_group
 
 
 if __name__ == "__main__":
-    server_obj = Server()
-    excel_obj = Excel()
-    server_obj.start_server_and_create_index(excel_obj)
+    server = Server()
+    excel = Excel()
+    server.create_index_html(excel)
+    pprint(excel.get_grouped_wines())
+    # server.main()
