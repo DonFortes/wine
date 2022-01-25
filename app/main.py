d72a52f9da3d2564bf2dc8969bc278fad441bc3e
Павел <paul-36@mail.ru>
2022-01-25 19:54:42 +0300

    Written tests for Excel table.

--- app/main.py
+++ app/main.py
@@ -6,46 +6,59 @@ import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 START_YEAR = 1920
-FILE_NAME = "excel_files/wines.xlsx"
+FILE_PATH = "../excel_files/wines.xlsx"
 SHEET_NAME = "Лист1"
 
 
 class Server:
-    def __init__(self):
+
+    """Class to create index.html and start the server."""
+
+    def __init__(self, start_year):
         self.env = Environment(
-            loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+            loader=FileSystemLoader(""), autoescape=select_autoescape(["html", "xml"])
         )
         self.template = self.env.get_template("template.html")
-        self.start_year = START_YEAR
+        self.start_year = start_year
         self.year_now = datetime.datetime.now().year
         self.age = self.year_now - self.start_year
 
     def create_index_html(self, excel):
+
+        """Creates index.html."""
+
         rendered_page = self.template.render(
             winery_age=self.age,
-            grouped_wines=excel.get_grouped_wines(),
+            grouped_wines=excel.create_grouped_wines(),
         )
         with open("index.html", "w", encoding="utf8") as file:
             file.write(rendered_page)
 
     def main(self):
+
+        """Starts the server."""
+
         _server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
         return _server.serve_forever()
 
 
 class Excel:
-    def __init__(self):
-        self.excel_name = FILE_NAME
-        self.sheet_name = SHEET_NAME
+
+    """Class for work with excel files."""
+
+    def __init__(self, excel_path, sheet_name):
+        self.excel_path = excel_path
+        self.sheet_name = sheet_name
         self.wines = pandas.read_excel(
-            self.excel_name,
+            self.excel_path,
             self.sheet_name,
             na_filter=False,
         ).to_dict(orient="records")
-        # Меня просто учили, что дробить на части - это лучше, чем
-        # делать многосоставные "предложения". Проще дебажить в случае чего.
 
-    def get_grouped_wines(self):
+    def create_grouped_wines(self):
+
+        """Creates groups of drinks."""
+
         wines_sorted_by_group = defaultdict(list)
         for wine in self.wines:
             wines_sorted_by_group[wine["Категория"]].append(wine)
@@ -53,7 +66,7 @@ class Excel:
 
 
 if __name__ == "__main__":
-    server = Server()
-    excel = Excel()
+    server = Server(START_YEAR)
+    excel = Excel(FILE_PATH, SHEET_NAME)
     server.create_index_html(excel)
     server.main()
---
+++ tests/test_excel_file.py
@@ -0,0 +1,47 @@
+from app import main
+
+FILE_PATH = "excel_files/wines.xlsx"
+SHEET_NAME = "Лист1"
+excel = main.Excel(FILE_PATH, SHEET_NAME)
+
+
+def test_category_in_table():
+    grouped_wines_params = list(excel.create_grouped_wines().items())[0][1][0]
+    assert (
+        "Категория" in grouped_wines_params.keys()
+    ), "Проверьте, что столбец 'Категория' есть в вашей таблице"
+
+
+def test_name_in_table():
+    grouped_wines_params = list(excel.create_grouped_wines().items())[0][1][0]
+    assert (
+        "Категория" in grouped_wines_params.keys()
+    ), "Проверьте, что столбец 'Категория' есть в вашей таблице"
+
+
+def test_sort_in_table():
+    grouped_wines_params = list(excel.create_grouped_wines().items())[0][1][0]
+    assert (
+        "Сорт" in grouped_wines_params.keys()
+    ), "Проверьте, что столбец 'Сорт' есть в вашей таблице"
+
+
+def test_price_in_table():
+    grouped_wines_params = list(excel.create_grouped_wines().items())[0][1][0]
+    assert (
+        "Цена" in grouped_wines_params.keys()
+    ), "Проверьте, что столбец 'Цена' есть в вашей таблице"
+
+
+def test_image_in_table():
+    grouped_wines_params = list(excel.create_grouped_wines().items())[0][1][0]
+    assert (
+        "Картинка" in grouped_wines_params.keys()
+    ), "Проверьте, что столбец 'Картинка' есть в вашей таблице"
+
+
+def test_stock_in_table():
+    grouped_wines_params = list(excel.create_grouped_wines().items())[0][1][0]
+    assert (
+        "Акция" in grouped_wines_params.keys()
+    ), "Проверьте, что столбец 'Акция' есть в вашей таблице"
